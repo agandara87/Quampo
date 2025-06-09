1268d2eee68ef61798acf22d1984d281d8c32977
agandara87 <161074964+agandara87@users.noreply.github.com>
2025-06-09 17:01:01 -0300

    quampo_backend.py

--- quampo_backend.py
+++ quampo_backend.py
@@ -9,7 +9,7 @@ import os
 import requests
 from datetime import datetime
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")  # ¡Clave correcta!
+#openai.api_key = os.environ.get("OPENAI_API_KEY")  # ¡Clave correcta!
 OPENWEATHER_API_KEY = "fa977f2964c89bf890b1502681dfa742"
 
 system_prompt_llm = """Sos un asesor técnico agrónomo digital que trabaja para Quampo...
